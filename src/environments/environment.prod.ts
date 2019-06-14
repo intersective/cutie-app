91fa4a15f737cb5638c6d29d079aa76e292433ee
Shawn <shawn@practera.com>
2019-06-14 10:41:04 +0800

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   demo: false,
+  appkey: 'b11e7c189b',
+  pusherKey: '255f010d210933ca7675',
+  env: 'live',
+  APIEndpoint: 'https://127.0.0.1:8081/',
+  APIEndpointOld: 'https://127.0.0.1:8080/',
 };
---
+++ src/environments/environment.sandbox.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  demo: false,
+  appkey: 'b11e7c189b',
+  pusherKey: '255f010d210933ca7675',
+  env: 'sandbox',
+  APIEndpoint: 'https://sandbox-cutie.api.practera.com/',
+  APIEndpointOld: 'https://sandbox.practera.com/',
+};
