183104a4f4212e3416d38147070967fcc5c6019b
Barry Chen <barry@intersective.com>
2019-06-18 19:38:20 +1000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   appkey: 'b11e7c189b',
   pusherKey: '255f010d210933ca7675',
   env: 'live',
-  APIEndpoint: 'https://127.0.0.1:8081/',
-  APIEndpointOld: 'https://127.0.0.1:8080/',
+  APIEndpoint: 'https://cutie.api.practera.com/',
+  APIEndpointOld: 'https://api.practera.com/',
 };
