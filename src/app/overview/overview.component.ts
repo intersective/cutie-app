4fb9a038d298612f71fdd3809308795d679f3756
Shawn <shawn@practera.com>
2021-02-09 13:36:55 +0800

    [CORE-4211]

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -25,6 +25,18 @@ export class OverviewComponent implements OnInit {
       value: ''
     }
   ];
+  sortList = [
+    'created time',
+    'participant count',
+    'mentor count',
+    'recent active participants',
+    'recent active mentors',
+    'feedback loops completed',
+    'on-track/off-track',
+    'feedback quality score',
+  ];
+  sortDesc = true;
+  sortBy = this.sortList[0];
   tags = [];
   types = ['all'];
   status = 'all';
@@ -39,9 +51,7 @@ export class OverviewComponent implements OnInit {
 
   ngOnInit() {
     this.service.getExperiences().subscribe(res => {
-      this.experiences = res;
       this.experiencesRaw = res;
-      this.calculateStatistics();
       // get all tags
       this.tags = [];
       res.forEach(exp => {
@@ -63,10 +73,116 @@ export class OverviewComponent implements OnInit {
       // get all types
       this.types = [...['all'], ...res.map(exp => exp.type)];
       this.types = [...new Set(this.types)];
+      this.filterAndOrder();
     });
   }
 
-  calculateStatistics() {
+  filterAndOrder() {
+    this.experiences = JSON.parse(JSON.stringify(this.experiencesRaw));
+    this._filterByTag();
+    this._filterByStatus();
+    this._filterByType();
+    this._sort();
+    this._calculateStatistics();
+  }
+
+  private _filterByTag() {
+    const activeTagIds = this.tags.filter(t => t.active).map(t => t.id);
+    if (!activeTagIds.length) {
+      return;
+    }
+    this.experiences = this.experiences.filter(exp => exp.tags.find(t => activeTagIds.includes(t.id)));
+  }
+
+  private _filterByStatus() {
+    if (this.status === 'all') {
+      return;
+    }
+    this.experiences = this.experiences.filter(exp => exp.status === this.status);
+  }
+
+  private _filterByType() {
+    if (this.type === 'all') {
+      return;
+    }
+    this.experiences = this.experiences.filter(exp => exp.type === this.type);
+  }
+
+  private _sort() {
+    const index = this.sortList.findIndex(s => s === this.sortBy);
+    switch (index) {
+      case 0:
+        if (!this.sortDesc) {
+          this.experiences.reverse();
+        }
+        break;
+
+      case 1:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.registeredUserCount.participant > b.statistics.registeredUserCount.participant ? -1 : 1;
+          }
+          return a.statistics.registeredUserCount.participant < b.statistics.registeredUserCount.participant ? -1 : 1;
+        });
+        break;
+
+      case 2:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.registeredUserCount.mentor > b.statistics.registeredUserCount.mentor ? -1 : 1;
+          }
+          return a.statistics.registeredUserCount.mentor < b.statistics.registeredUserCount.mentor ? -1 : 1;
+        });
+        break;
+
+      case 3:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.activeUserCount.participant > b.statistics.activeUserCount.participant ? -1 : 1;
+          }
+          return a.statistics.activeUserCount.participant < b.statistics.activeUserCount.participant ? -1 : 1;
+        });
+        break;
+
+      case 4:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.activeUserCount.mentor > b.statistics.activeUserCount.mentor ? -1 : 1;
+          }
+          return a.statistics.activeUserCount.mentor < b.statistics.activeUserCount.mentor ? -1 : 1;
+        });
+        break;
+
+      case 5:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.feedbackLoopCompleted > b.statistics.feedbackLoopCompleted ? -1 : 1;
+          }
+          return a.statistics.feedbackLoopCompleted < b.statistics.feedbackLoopCompleted ? -1 : 1;
+        });
+        break;
+
+      case 6:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.onTrackRatio > b.statistics.onTrackRatio ? -1 : 1;
+          }
+          return a.statistics.onTrackRatio < b.statistics.onTrackRatio ? -1 : 1;
+        });
+        break;
+
+      case 7:
+        this.experiences.sort((a, b) => {
+          if (this.sortDesc) {
+            return a.statistics.reviewRatingAvg > b.statistics.reviewRatingAvg ? -1 : 1;
+          }
+          return a.statistics.reviewRatingAvg < b.statistics.reviewRatingAvg ? -1 : 1;
+        });
+        break;
+    }
+  }
+
+  private _calculateStatistics() {
     let liveExpCount = 0;
     let activeUsers = 0;
     let totalUsers = 0;
@@ -92,44 +208,6 @@ export class OverviewComponent implements OnInit {
     this.stats[1].value = totalUsers ? `${ Math.round(activeUsers * 100 / totalUsers) }%` : '0%';
     this.stats[2].value = fbStarted ? `${ fbCompleted }/${ fbStarted }` : '0/0';
     this.stats[3].value = `${ Math.round(reviewRatingAvg * 100) }%`;
-  }
-
-  filterByTag(tag: Tag) {
-    const index = this.tags.findIndex(t => t.id === tag.id);
-    this.tags[index].active = !tag.active;
-    const activeTagIds = this.tags.filter(t => t.active).map(t => t.id);
-    if (!activeTagIds.length) {
-      this.experiences = this.experiencesRaw;
-    } else {
-      this.experiences = this.experiencesRaw.filter(exp => exp.tags.find(t => activeTagIds.includes(t.id)));
-    }
-    this.calculateStatistics();
-  }
-
-  filterByStatus(status: string) {
-    if (this.status === status) {
-      return;
-    }
-    if (status === 'all') {
-      this.experiences = this.experiencesRaw;
-    } else {
-      this.experiences = this.experiencesRaw.filter(exp => exp.status === status);
-    }
-    this.status = status;
-    this.calculateStatistics();
-  }
-
-  filterByType(type: string) {
-    if (this.type === type) {
-      return;
-    }
-    if (type === 'all') {
-      this.experiences = this.experiencesRaw;
-    } else {
-      this.experiences = this.experiencesRaw.filter(exp => exp.type === type);
-    }
-    this.type = type;
-    this.calculateStatistics();
   }
 
 }
