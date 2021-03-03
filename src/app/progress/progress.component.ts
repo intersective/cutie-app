0204db6184e4cda43e8f9a0659e24c5cc93a1202
Shawn <shawn@practera.com>
2021-03-03 13:46:47 +0800

    [CUT-84]

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./progress.component.scss'],
 })
 export class ProgressComponent implements OnInit {
-
+  @Input() skeletonOnly: boolean;
   constructor() { }
 
   ngOnInit() {}
