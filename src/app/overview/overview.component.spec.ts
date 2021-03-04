6f8a8428c0aa0df2e0b5721c13224b3833f00d28
Shawn <shawn@practera.com>
2021-03-04 12:28:29 +0800

    unit test

--- src/app/overview/overview.component.spec.ts
+++ src/app/overview/overview.component.spec.ts
@@ -1,16 +1,39 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
+import { SharedModule } from '@shared/shared.module';
 import { OverviewComponent } from './overview.component';
+import { OverviewService } from './overview.service';
+import { UtilsService } from '@services/utils.service';
+import { PopupService } from '@shared/popup/popup.service';
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
   let fixture: ComponentFixture<OverviewComponent>;
+  const overviewSpy = jasmine.createSpyObj('OverviewService', ['getExperiences']);
+  const utilsSpy = jasmine.createSpyObj('UtilsService', ['getEvent']);
+  const popupSpy = jasmine.createSpyObj('PopupService', ['showCreateExp']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [SharedModule],
       declarations: [ OverviewComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: OverviewService,
+          useValue: overviewSpy
+        },
+        {
+          provide: UtilsService,
+          useValue: utilsSpy
+        },
+        {
+          provide: PopupService,
+          useValue: popupSpy
+        },
+      ]
     })
     .compileComponents();
   }));
@@ -18,10 +41,14 @@ describe('OverviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(OverviewComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    overviewSpy.getExperiences = jasmine.createSpy().and.returnValue(of());
+    utilsSpy.getEvent = jasmine.createSpy().and.returnValue(of());
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+
 });
