98715521428b5add32d19e86296a33ab9037931e
Richard Watts-Seale <rwattsseale@gmail.com>
2021-04-02 10:10:34 +1100

    CORE-4340 Flesh out skeletons. Fix isLarge bug on category card. Use the ionic menu tags. Adjust some css

--- src/app/template-library/template-card/template-card.component.ts
+++ src/app/template-library/template-card/template-card.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Template} from '../../template-library/template-library.service';
 
 @Component({
-  selector: 'app-template-experience-card',
+  selector: 'app-template-card',
   templateUrl: './template-card.component.html',
   styleUrls: ['./template-card.component.scss'],
 })
