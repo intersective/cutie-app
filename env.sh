c5788d2bf935065180b0df4cdfc4d7a752122079
Sunil Badyankal <sunilsbcloud@gmail.com>
2021-04-28 21:43:08 +1000

    new way aws deploy alpha+beta

--- env.sh
+++ env.sh
@@ -2,6 +2,5 @@
 while IFS='=' read -r name value ; do
     if [[ $name == 'CUSTOM_'* ]]; then
         sed -i "s#<$name>#${!name}#g" src/environments/environment.custom.ts
-        echo "replacing <$name> to ${!name}"
     fi
 done < <(env)
