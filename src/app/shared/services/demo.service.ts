52fa804206e5fff240547d22b958fdc69842584a
Richard Watts-Seale <rwattsseale@gmail.com>
2021-06-22 11:30:30 +1000

    Wrap the description in a div to handle the line clamping better in different browsers. Add padding to the bottom of the container

--- src/app/shared/services/demo.service.ts
+++ src/app/shared/services/demo.service.ts
@@ -492,7 +492,7 @@ export class DemoService {
     {
       uuid: '000f562e-0ed0-4afe-af53-7a8d20558ce1',
       name: 'Consulting Project Experience',
-      description: `Practera is the leading platform to power high quality experiential learning programs.<br/>Deliver experiential learning programs at larger scale and lower cost<br/>Customisable platform to author, launch & manage programs<br/>Connect students to industry projects, internships & experiences<br/>Expert course design, configuration and deployment services`,
+      description: `<p>Practera is the leading platform to power high quality experiential learning programs.<br/>Deliver experiential learning programs at larger scale and lower cost<br/>Customisable platform to author, launch & manage programs<br/>Connect students to industry projects, internships & experiences<br/>Expert course design, configuration and deployment services<\p><p>This is more content</p>`,
       leadImageUrl: 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2252&q=80',
       leadVideoUrl: 'https://www.filepicker.io/api/file/2i8R83noQbaJcbA9woIL',
       type: 'Work Simulation',
