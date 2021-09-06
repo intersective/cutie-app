cf8ac1ffc250ee1941fa24afd4e9faa58b948b33
Shawn <shawn@practera.com>
2021-09-06 12:05:36 +0800

    [CORE-4792]

--- src/app/auth/auth.component.spec.ts
+++ src/app/auth/auth.component.spec.ts
@@ -6,16 +6,18 @@ import { AuthComponent } from './auth.component';
 import { AuthService } from './auth.service';
 import { NotificationService } from '@services/notification.service';
 import { Router, ActivatedRoute, convertToParamMap } from '@angular/router';
+import { PusherService } from '@app/shared/pusher/pusher.service';
 
 describe('AuthComponent', () => {
   let component: AuthComponent;
   let fixture: ComponentFixture<AuthComponent>;
-  let routerSpy, activeRouterSpy, authServiceSpy, notificationServiceSpy;
+  let routerSpy, activeRouterSpy, authServiceSpy, notificationServiceSpy, pusherServiceSpy;
 
   beforeEach(async(() => {
     routerSpy = jasmine.createSpyObj('Router', ['navigate']);
     authServiceSpy = jasmine.createSpyObj('AuthService', ['directLogin']);
     notificationServiceSpy = jasmine.createSpyObj('NotificationService', ['alert']);
+    pusherServiceSpy = jasmine.createSpyObj('PusherService', ['initialise']);
     TestBed.configureTestingModule({
       declarations: [ AuthComponent ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -35,6 +37,7 @@ describe('AuthComponent', () => {
         },
         { provide: AuthService, useValue: authServiceSpy },
         { provide: NotificationService, useValue: notificationServiceSpy },
+        { provide: PusherService, useValue: pusherServiceSpy }
       ]
     })
     .compileComponents();
@@ -45,6 +48,7 @@ describe('AuthComponent', () => {
     fixture = TestBed.createComponent(AuthComponent);
     component = fixture.componentInstance;
     activeRouterSpy = TestBed.inject(ActivatedRoute);
+    pusherServiceSpy.initialise = jasmine.createSpy().and.returnValue(of({}));
     fixture.detectChanges();
   });
 
