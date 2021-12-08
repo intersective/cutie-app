b18327d5f518bd7e0c1bd705c3da3c7f1e7b5d57
sasangachathumal <chathumal.sasanga02@gmail.com>
2021-12-08 11:42:02 +0530

    [CUT-118] create scss variables for new colors. create new color class for font colors. update styles for header section. added icons to experience types.

--- src/app/onboarding/type-of-experience/type-of-experience.page.ts
+++ src/app/onboarding/type-of-experience/type-of-experience.page.ts
@@ -12,35 +12,35 @@ export class TypeOfExperiencePage {
     {
       title: 'Industry Project',
       description: 'Immerse learners in real-world projects across multiple industries. Increase skills through reflections and a critical thinking framework.',
-      icon: '',
+      icon: 'fa-building',
       tags: [],
       navigate: ['onboarding', 'details', 'industryProject']
     },
     {
       title: 'Internship',
       description: `Work experience guided by experts. Develop and refine skills, explore career path, gain confidence and improve learner's resume.`,
-      icon: '',
+      icon: 'fa-user-astronaut',
       tags: [],
       navigate: ['onboarding', 'details', 'internship']
     },
     {
       title: 'Work Simulation',
       description: 'Execute real-world simulations to immerse candidates in real scenarios. Useful experience for hiring or promotions processes.',
-      icon: '',
+      icon: 'fa-briefcase',
       tags: [],
       navigate: ['onboarding', 'details', 'workSimulation']
     },
     {
       title: 'Mentoring',
       description: 'Tap into the existing knowledge, skills, and experience of senior or high performing employees and transfer these skills to newer or less experienced employees.',
-      icon: '',
+      icon: 'fa-hands-helping',
       tags: [],
       navigate: ['onboarding', 'details', 'mentoring']
     },
     {
       title: 'Accelerator',
       description: 'Help learners or funders to find their product-market fit and scale their ventures. Simulate pitchs, investors meetings and networking skills.',
-      icon: '',
+      icon: 'fa-rocket',
       tags: [],
       navigate: ['onboarding', 'details', 'accelerator']
     }
