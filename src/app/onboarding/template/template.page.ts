9c9a6ff0aa8d6447d08da6f0edb44f021f95074a
sasangachathumal <chathumal.sasanga02@gmail.com>
2021-12-21 13:39:43 +0530

    [CUT-116] Added header icon to template page.

--- src/app/onboarding/template/template.page.ts
+++ src/app/onboarding/template/template.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OnboardingService, Template } from '../onboarding.service';
 
+import { StorageService } from '@services/storage.service';
+
 @Component({
   selector: 'app-template',
   templateUrl: './template.page.html',
@@ -11,13 +13,17 @@ export class TemplatePage implements OnInit {
   type: string;
   template: Template;
   durationIndex = 0;
+  projectIcon: string;
+
   constructor(
     private service: OnboardingService,
     private router: Router,
     private route: ActivatedRoute,
+    public storage: StorageService
   ) { }
 
   ngOnInit() {
+    this.projectIcon = this.storage.get('selectedProjectIcon');
     this.route.params.subscribe(params => {
       this.template = null;
       this.type = params.type;
