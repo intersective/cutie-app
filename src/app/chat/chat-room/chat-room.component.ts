7a1ccc2b6c2ca0d2d654ac7c17bce632ad4f0fe6
sasangachathumal <chathumal.sasanga02@gmail.com>
2022-08-15 15:59:58 +0530

    [CUT-134] formated chat messages elemets in HTML. added action buttons to delete and edit messages. added styles for action buttons. update styles to match formated elements. add logic to open delete and edit popups from chat room.

--- src/app/chat/chat-room/chat-room.component.ts
+++ src/app/chat/chat-room/chat-room.component.ts
@@ -5,11 +5,13 @@ import { StorageService } from '@services/storage.service';
 import { UtilsService } from '@services/utils.service';
 import { PusherService } from '@shared/pusher/pusher.service';
 import { FilestackService } from '@shared/filestack/filestack.service';
+import { PopupService } from '@shared/popup/popup.service';
 
 import { ChatService, ChatChannel, Message, MessageListResult } from '../chat.service';
 import { ChatPreviewComponent } from '../chat-preview/chat-preview.component';
 import { ChatInfoComponent } from '../chat-info/chat-info.component';
 import { ScheduleMessagePopupComponent } from '../schedule-message-popup/schedule-message-popup.component';
+import { EditScheduleMessagePopupComponent } from '../edit-schedule-message-popup/edit-schedule-message-popup.component';
 
 @Component({
   selector: 'app-chat-room',
@@ -59,7 +61,8 @@ export class ChatRoomComponent {
     private filestackService: FilestackService,
     private modalController: ModalController,
     private ngZone: NgZone,
-    public element: ElementRef
+    public element: ElementRef,
+    private popupService: PopupService
   ) {
     if (this.skeletonOnly) {
       return;
@@ -379,6 +382,19 @@ export class ChatRoomComponent {
       return 'received-messages no-avatar';
     }
     return 'received-messages';
+  }
+
+  getClassForMessageBody(message) {
+    if (!message.fileObject || !message.fileObject.mimetype ||
+      (!message.fileObject.mimetype.includes('image') && !message.fileObject.mimetype.includes('video'))) {
+      return '';
+    }
+    if (message.fileObject.mimetype && message.fileObject.mimetype.includes('video')) {
+      return 'video-attachment-container';
+    }
+    if (message.fileObject.mimetype && message.fileObject.mimetype.includes('image')) {
+      return 'image';
+    }
   }
 
   /**
@@ -708,6 +724,71 @@ export class ChatRoomComponent {
         this.chatChannel.scheduledMessageCount += 1;
         this.utils.broadcastEvent('chat:info-update', true);
       }
+    });
+  }
+
+  deleteMessage(messageUuid) {
+    this.popupService.showAlert({
+      header: 'Delete Message?',
+      message: 'Are you sure you want to delete this message.<br/>This action cannot be undone.',
+      cssClass: 'message-delete-alert',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Delete Message',
+          cssClass: 'danger',
+          handler: () => {
+            this.chatService.deleteChatMesage(messageUuid).subscribe(res => {
+              // this will update chat list
+              this.utils.broadcastEvent('chat:info-update', true);
+              /*
+              if we call loadMessages() again here it will call with newest cursor.
+              then if that new cursor is for next page, it will load new messages but old message/deleted message still in the list.
+              */
+              this.removeMessageFromList(messageUuid);
+            });
+          }
+        },
+      ]
+    });
+  }
+
+  removeMessageFromList(messageUuid) {
+    const deletedMessageIndex = this.messageList.findIndex(message => {
+      return message.uuid === messageUuid;
+    });
+    if (deletedMessageIndex === -1) {
+      return;
+    }
+    this.messageList.splice(deletedMessageIndex, 1);
+  }
+
+  async openEditMessagePopup(index) {
+    const modal = await this.modalController.create({
+      component: EditScheduleMessagePopupComponent,
+      cssClass: 'chat-schedule-message-popup',
+      componentProps: {
+        chatMessage: this.messageList[index],
+        channelName: this.chatChannel.name,
+        reScheduled: false
+      }
+    });
+    await modal.present();
+    modal.onWillDismiss().then((data) => {
+    /*
+      we can't call loadMessages() again here.
+      becouse it will call with cursor to get next set of messages.
+      and we can't make cursor null and call loadMessages().
+      becaouse the everytime user edit some messages messages get loan again.
+    */
+      if (data.data.updateSuccess && data.data.newMessageData) {
+        this.messageList[index].message = data.data.newMessageData;
+      }
+      // this will update chat list
+      this.utils.broadcastEvent('chat:info-update', true);
     });
   }
 
