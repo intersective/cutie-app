1ad4d03f7f3b73ccfbbe15f076547f1dcc88778b
Shawn <shawn@practera.com>
2022-08-29 16:28:05 +0800

    CUT-148

--- src/app/chat/chat-room/chat-room.component.ts
+++ src/app/chat/chat-room/chat-room.component.ts
@@ -82,7 +82,6 @@ export class ChatRoomComponent {
         receivedMessage.fileObject = fileObject;
         receivedMessage.preview = this.attachmentPreview(receivedMessage.fileObject);
       }
-      console.log(receivedMessage);
       if (receivedMessage.senderUuid &&
         this.storage.getUser().uuid &&
         receivedMessage.senderUuid === this.storage.getUser().uuid
